c8dee345d650ca1f8e439fac293271cb4f490240
natspindola <natspindola@hotmail.com>
2022-03-22 19:15:58 -0300

    editor

--- Menu.cs
+++ Menu.cs
@@ -64,7 +64,7 @@ namespace EditorHtml
         public static void HandleMenuOption(short option)
         {
             switch(option) {
-                case 1: Console.WriteLine("Editor"); break;
+                case 1: Editor.Show(); break;
                 case 2: Console.WriteLine("View"); break;
                 case 0: {
                     Console.Clear();
